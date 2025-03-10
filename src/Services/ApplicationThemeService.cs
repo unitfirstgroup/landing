0e154375fb7984c2a27d3f4b12d40005e67dd186
Anton <antonryzhkou@gmail.com>
2025-03-10 22:50:21 +0300

    ref

--- src/Services/ApplicationThemeService.cs
+++ src/Services/ApplicationThemeService.cs
@@ -7,13 +7,34 @@ namespace UnitFirst.Landing.Services;
 
 public class ApplicationThemeService : IApplicationThemeService
 {
+    private readonly IStringLocalizer<App> _localizer;
     private const string OrganizationId = "organizationId";
     private const string DarkMode = "darkMode";
 
     
     public ApplicationThemeService(IStringLocalizer<App> localizer)
     {
-        var s = localizer["GET_STARTED"];
+        _localizer = localizer ?? throw new ArgumentNullException();
+        Theme = new()
+        {
+            Organization = "UnitFirst",
+            Logo = "images/simple-logo.svg",
+            Dark = "dark",
+            NavItems = new ObservableCollection<NavItem>(new[]
+            {
+                new NavItem { Name = _localizer["SERVICES"].Value, Link = "./" },
+                new NavItem { Name = _localizer["LABORATORIES"].Value, Link = "./" },
+                new NavItem { Name = _localizer["CASES"].Value, Link = "./" },
+                new NavItem { Name = _localizer["BLOG"].Value, Link = "./" },
+                new NavItem { Name = _localizer["ABOUT"].Value, Link = "./" },
+                new NavItem { Name = _localizer["CONTACTS"].Value, Link = "./" }
+            }),
+            Background = new Dictionary<string, string>(new[]
+            {
+                new KeyValuePair<string, string>("white", "bg-blue-400"),
+                new KeyValuePair<string, string>("dark", "bg-green-500")
+            })
+        };
     }
 
     public void DarkModeSwitch(Theme theme)
@@ -24,29 +45,6 @@ public class ApplicationThemeService : IApplicationThemeService
             $"Organization theme changed. OrganizationName: {theme.Organization}. DarkMode: {theme.Dark == "dark"}");
     }
 
-    public Theme Theme { get; } = new()
-    {
-        Organization = "UnitFirst",
-        Logo = "images/simple-logo.svg",
-        Dark = "dark",
-        NavItems = new ObservableCollection<NavItem>(new[]
-        {
-            new NavItem { Name = "Services", Link = "./" },
-            new NavItem { Name = "Laboratories", Link = "./" },
-            new NavItem { Name = "Cases", Link = "./" },
-            new NavItem { Name = "Blog", Link = "./" },
-            new NavItem { Name = "About the company", Link = "./" },
-            new NavItem { Name = "Contacts", Link = "./" }
-        }),
-        Background = new Dictionary<string, string>(new[]
-        {
-            new KeyValuePair<string, string>("white", "bg-blue-400"),
-            new KeyValuePair<string, string>("dark", "bg-green-500")
-        })
-    };
+    public Theme Theme { get; }
     
-    public void DarkModeSwitch()
-    {
-        throw new NotImplementedException();
-    }
 }
--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,14 +1,5 @@
 module.exports = {
   content: ["./**/*.{razor,html,cshtml}", "./node_modules/flowbite/**/*.js"],
-  theme: {
-    extend: {
-      fontFamily: {
-        display: "Oswald, ui-serif",
-        sans: "Passion One, sans",
-        serif: 'Merriweather", "serif'
-      }
-    }
-  },
   plugins: [
     require("flowbite/plugin")({
       charts: true,
