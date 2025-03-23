df0883c85da9e4b7b7cfff781ba253e411ac5408
Anton <antonryzhkou@gmail.com>
2025-03-23 18:20:41 +0300

    social

---
+++ src/Models/FooterModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace UnitFirst.Landing.Models
+{
+    public partial class FooterModel : ObservableObject
+    {
+        //[ObservableProperty] private ObservableCollection<NavItem> _navItems;
+
+        [ObservableProperty] private string _socialNetworksDescription;
+
+        [ObservableProperty] private string _organizationManifest;
+    }
+}
--- src/ViewModels/FooterViewModel.cs
+++ src/ViewModels/FooterViewModel.cs
@@ -1,15 +1,29 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
 using UnitFirst.Landing.Interfaces;
+using UnitFirst.Landing.Models;
 
 namespace UnitFirst.Landing.ViewModels;
 
-public partial class FooterViewModel : ViewModelBase
+public partial class FooterViewModel(IApplicationStateService applicationStateService,
+        IApplicationThemeService applicationThemeService, NavigationManager navigationManager,
+        IStringLocalizer<App> localizer)
+    : ViewModelBase(applicationStateService, applicationThemeService, navigationManager)
 {
-    public FooterViewModel(IApplicationStateService applicationStateService,
-        IApplicationThemeService applicationThemeService, NavigationManager navigationManager) : base(
-        applicationStateService, applicationThemeService, navigationManager)
+    [ObservableProperty]
+    private FooterModel _model;
+
+    public override Task OnInitializedAsync()
     {
+        Model = new FooterModel
+        {
+            SocialNetworksDescription = localizer["SOCIAL_NETWORKS_DESC"].Value,
+            OrganizationManifest = localizer["ORGANIZATION_MANIFEST"].Value
+        };
+
+        return base.OnInitializedAsync();
     }
 
     [RelayCommand]
