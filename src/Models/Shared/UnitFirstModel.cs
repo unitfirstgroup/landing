3fe65f65dd2afbbbba0e46f3a7056bb54744a7cc
Anton <antonryzhkou@gmail.com>
2025-04-12 14:05:43 +0300

    ref

---
+++ src/Models/Shared/UnitFirstModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace UnitFirst.Landing.Models.Shared
+{
+    public partial class UnitFirstModel : ObservableObject
+    {
+        [ObservableProperty] private bool _hide;
+        [ObservableProperty] private bool _focus;
+    }
+}
