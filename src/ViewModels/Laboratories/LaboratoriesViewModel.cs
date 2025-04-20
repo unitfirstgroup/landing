0861d5ef7368aa7387b75d2e0269e803ac26a39f
Anton <antonryzhkou@gmail.com>
2025-04-21 00:05:38 +0300

    ref

--- src/ViewModels/Laboratories/LaboratoriesViewModel.cs
+++ src/ViewModels/Laboratories/LaboratoriesViewModel.cs
@@ -1,8 +1,12 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Localization;
+using System.Collections.ObjectModel;
 using UnitFirst.Landing.Interfaces;
+using UnitFirst.Landing.Models;
 using UnitFirst.Landing.Models.Laboratories;
+using UnitFirst.Landing.Models.Shared;
 
 namespace UnitFirst.Landing.ViewModels.Laboratories;
 
@@ -15,9 +19,49 @@ public partial class LaboratoriesViewModel(IApplicationStateService applicationS
     : MultipleItemsViewModelBase<LaboratoryModel>(applicationStateService, applicationThemeService, navigationManager,
         localizer, searchService, dataService)
 {
+
+    [ObservableProperty] private MultipleItemsModelBase<LaboratoryModel> _model;
+
+    public override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+
+        _model = new MultipleItemsModelBase<LaboratoryModel>()
+        {
+            Filter = new FilterModel()
+            {
+                Dark = Theme.Dark,
+                Tags = new ObservableCollection<TagModel>(_tags),
+                SearchModel = new SearchModel()
+                {
+                    RaiseSearchCommand = SearchCommand,
+                    Placeholder = "Search digital laboratory for your requirements..."
+                }
+            },
+            Filtered = new ObservableCollection<LaboratoryModel>(_items)
+        };
+    }
+
     [RelayCommand]
     public void GetDetails()
     {
         Console.WriteLine("Get Details");
     }
+
+
+    public void UpdateDataGrid(IEnumerable<LaboratoryModel> filtered)
+    {
+        Model.Filtered.Clear();
+        foreach (var laboratoryModel in filtered) Model.Filtered.Add(laboratoryModel);
+        NotifyStateChanged();
+    }
+
+    [RelayCommand]
+    public void Search()
+    {
+        var filtered = searchService.Search(_items, Model.Filter);
+
+        UpdateDataGrid(filtered);
+    }
+
 }
--- src/ViewModels/MultipleItemsViewModelBase.cs
+++ src/ViewModels/MultipleItemsViewModelBase.cs
@@ -1,8 +1,5 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Localization;
-using System.Collections.ObjectModel;
 using UnitFirst.Landing.Interfaces;
 using UnitFirst.Landing.Models;
 using UnitFirst.Landing.Models.Shared;
@@ -21,39 +18,11 @@ public abstract partial class MultipleItemsViewModelBase<TItem>(IApplicationStat
     protected List<TItem> _items;
     protected List<TagModel> _tags;
 
-    [ObservableProperty] private MultipleItemsModelBase<TItem> _model;
-
     public override Task OnInitializedAsync()
     {
         _items = dataService.LoadData();
         _tags = dataService.LoadTags();
 
-        _model = new MultipleItemsModelBase<TItem>()
-        {
-            Filter = new FilterModel()
-            {
-                Dark = Theme.Dark,
-                Tags = new ObservableCollection<TagModel>(_tags),
-                SearchModel = new SearchModel() { RaiseSearchCommand = SearchCommand }
-            },
-            Filtered = new ObservableCollection<TItem>(_items)
-        };
-
         return base.OnInitializedAsync();
-    }
-
-    public void UpdateDataGrid(IEnumerable<TItem> filtered)
-    {
-        Model.Filtered.Clear();
-        foreach (var laboratoryModel in filtered) Model.Filtered.Add(laboratoryModel);
-        NotifyStateChanged();
-    }
-
-    [RelayCommand]
-    public void Search()
-    {
-        var filtered = searchService.Search(_items, Model.Filter);
-
-        UpdateDataGrid(filtered);
     }
 }
