4e78ea7534bb989780de0229c85e39d2856f827d
Anton <antonryzhkou@gmail.com>
2025-04-28 14:10:59 +0300

    ref

--- src/Models/Laboratories/LaboratoryModel.cs
+++ src/Models/Laboratories/LaboratoryModel.cs
@@ -4,10 +4,7 @@ namespace UnitFirst.Landing.Models.Laboratories;
 
 public partial class LaboratoryModel : BaseModel
 {
-    [ObservableProperty] private string _description;
     [ObservableProperty] private string _image;
-    [ObservableProperty] private string _name;
     [ObservableProperty] private string _url;
     [ObservableProperty] private string _urlText;
-    public override string SearchValue => Name;
 }
