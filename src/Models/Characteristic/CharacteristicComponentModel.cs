fae0e234e18ba209ca2d0ec0c9e502725640ca86
Anton <antonryzhkou@gmail.com>
2025-05-01 10:57:47 +0300

    ref

---
+++ src/Models/Characteristic/CharacteristicComponentModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace UnitFirst.Landing.Models.Characteristic;
+
+public partial class CharacteristicComponentModel : BaseModel
+{
+    [ObservableProperty] private string _title;
+    [ObservableProperty] private ObservableCollection<CharacteristicModel> _characteristicItems;
+    [ObservableProperty] private string _dark;
+}
---
+++ src/Models/Characteristic/CharacteristicModel.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace UnitFirst.Landing.Models.Characteristic;
+
+public partial class CharacteristicModel : BaseModel
+{
+    [ObservableProperty] private string _header;
+    [ObservableProperty] private string _details;
+    [ObservableProperty] private string _value;
+    [ObservableProperty] private string _benefit;
+}
