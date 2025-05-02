334477ae8c2b54807420b3e0900382b29f74a995
Anton <antonryzhkou@gmail.com>
2025-05-02 22:52:44 +0300

    ref

--- src/Models/CloneBaseModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace UnitFirst.Landing.Models;
-
-public partial class CloneBaseModel : BaseModel
-{
-    [ObservableProperty] private string _name;
-    public override string SearchValue => Name;
-}
