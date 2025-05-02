588c8d64ffb6a4e4eae51c54d7e6be4a0bc755a5
Anton <antonryzhkou@gmail.com>
2025-05-02 22:46:25 +0300

    ref

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -3,11 +3,24 @@ const colors = require("tailwindcss/colors");
 module.exports = {
     content: ["./**/*.{razor,html,cshtml}", "./node_modules/flowbite/**/*.js"],
     theme: {
+        colors: {
+            zela: {
+                100: '#ff0000',
+                300: '#00ff00',
+                900: '#0000ff',
+            },
+        },
         extend: {
             fontFamily: {
                 display: "Roboto, ui-serif",
                 sans: "Roboto, sans",
                 serif: 'Roboto", "serif'
+            },
+            colors: {
+                'primary-white': '#1fb6ff',
+                'primary-dark': '#7e5bef',
+                'primary-accent-white': '#ff49db',
+                'primary-accent-dark': '#ff7849',
             }
         }
     },
