c1bd44bd1cdeed729fde378986fc4e6abd243e63
Anton <antonryzhkou@gmail.com>
2025-05-09 23:04:26 +0300

    ref

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
                 serif: 'Roboto", "serif'
             },
             colors: {
-                'white-main': '#fff', // white
+                'white-main': 'oklch(0.985 0.001 106.423)', // #fff white
                 'dark-main': 'oklch(0.21 0.034 264.665)', // gray-900
                 'white-action': 'oklch(0.586 0.253 17.585)', // rose-600
                 'dark-action': 'oklch(0.852 0.199 91.936)' // yellow-400
