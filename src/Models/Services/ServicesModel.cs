c7312df9a9219b2acbb638cc5bca67be2a5a9fd2
Anton <antonryzhkou@gmail.com>
2025-05-12 22:26:05 +0300

    ref

--- src/Models/Services/ServicesModel.cs
+++ src/Models/Services/ServicesModel.cs
@@ -5,7 +5,15 @@ namespace UnitFirst.Landing.Models.Services;
 
 public partial class ServicesModel : ObservableObject
 {
-    [ObservableProperty] private ObservableCollection<ServiceModel> _services;
+    [ObservableProperty] private string _accentWord;
 
-    [ObservableProperty] private string _socialNetworksDescription;
+    [ObservableProperty] private string _categoryAccentWord;
+
+    [ObservableProperty] private string _categoryTitle;
+
+    [ObservableProperty] private string _description;
+    
+    [ObservableProperty] private string _title;
+
+    [ObservableProperty] private ObservableCollection<ServiceModel> _services;
 }
