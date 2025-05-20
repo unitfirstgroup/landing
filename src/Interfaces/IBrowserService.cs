087efae55999f92991a7d6750697c5942062f12a
Anton <antonryzhkou@gmail.com>
2025-05-20 22:26:21 +0300

    ref

--- src/Interfaces/IBrowserService.cs
+++ src/Interfaces/IBrowserService.cs
@@ -4,18 +4,27 @@ namespace UnitFirst.Landing.Interfaces;
 
 public interface IBrowserService
 {
-    public Task Initialize();
-    public Task<BrowserDimension> GetDimensions();
-    
     /// <summary>
-    /// Show alert popup
+    ///     Initialize service
+    /// </summary>
+    /// <returns></returns>
+    public Task Initialize();
+
+    /// <summary>
+    ///     Get browser screen dimensions
+    /// </summary>
+    /// <returns></returns>
+    public Task<BrowserDimension> GetDimensions();
+
+    /// <summary>
+    ///     Show alert popup
     /// </summary>
     /// <param name="alertText"></param>
     /// <returns></returns>
     public Task ShowAlert(string alertText);
-    
+
     /// <summary>
-    /// Switch dark mode
+    ///     Switch dark mode
     /// </summary>
     /// <returns>false - white, true - dark</returns>
     public Task<bool> DarkModeSwitch();
