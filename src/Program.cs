7ea59547f1b18bad1e8a7017aac71fa41fb97b4d
Anton <antonryzhkou@gmail.com>
2025-05-21 23:33:20 +0300

    ref

--- src/Program.cs
+++ src/Program.cs
@@ -11,6 +11,7 @@ using UnitFirst.Landing.Interfaces;
 using UnitFirst.Landing.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
@@ -18,11 +19,9 @@ builder.Services.AddLocalization(options =>
 {
     options.ResourcesPath = "Resources";
 });
-
 builder.Services.AddViewModels(builder.Configuration);
 builder.Services.AddSearchServices(builder.Configuration);
 builder.Services.AddDataServices(builder.Configuration);
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredLocalStorageAsSingleton(config =>
 {
     config.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
